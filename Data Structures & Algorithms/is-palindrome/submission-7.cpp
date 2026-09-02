f4e0585d90ae96582654c88b2747bcdd36b3b111
Mohamed_Salem <m7amed.salem@gmail.com>
2026-09-02 20:51:38 +0300

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        string copy;
+        for (auto ch : s) {
+            if (isalnum(static_cast<unsigned char>(ch)))
+                copy += tolower(static_cast<unsigned char>(ch));
+        }
+        int j = copy.length() - 1;
+        int i = 0;
+        while (i < j) {
+            if (copy[i] != copy[j]) return false;
+            j--;
+            i++;
+        }
+        return true;
+        // return equal(copy.begin(),copy.end(),copy.rbegin());
+    }
+};
