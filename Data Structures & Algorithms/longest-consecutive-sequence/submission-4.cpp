dc849a25be430d58d5387b9ae51491b4f685832b
Mohamed_Salem <m7amed.salem@gmail.com>
2026-09-02 18:34:02 +0300

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        // int len=0,maxlen=0;
+        // unordered_set<int>myset(nums.begin(),nums.end());
+        // for(int num : myset){
+        //     if(!myset.count(num-1)){
+        //         int curr=num;
+        //         len=1;
+        //         while(myset.count(curr+1)){
+        //             len++;
+        //             curr++;
+        //         }
+        //     }
+        //     maxlen=max(maxlen,len);
+
+        // }
+        // return maxlen;
+        int len=0,maxlen=0;
+        unordered_set<int>myset(nums.begin(),nums.end());
+        for(auto num: myset){
+            if(!myset.count(num-1)){
+                int curr=num;
+                len=1;
+                while(myset.count(curr+1)){
+                    len++;
+                    curr++;
+                }
+                            maxlen=max(len,maxlen);
+
+            }
+        }
+        return maxlen;
+
+
+    }
+};
