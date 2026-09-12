d7ed9d3071dc13372a24af71e9d59697ee84644d
Mohamed_Salem <m7amed.salem@gmail.com>
2026-09-12 15:43:20 +0300

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.cpp
@@ -0,0 +1,60 @@
+class Solution {
+   public:
+    // vector<int> memo;
+
+    // int dfs(vector<int>& nums, int i) {
+    //     if (i >= nums.size()) return 0;
+    //     if (memo[i] != -1) return memo[i];
+
+    //     int rob = nums[i] + dfs(nums, i + 2);
+    //     int skip = dfs(nums, i + 1);
+    //     memo[i] = max(rob, skip);
+    //     return memo[i];
+    // }
+
+        int solve(vector<int>& nums, int start, int end) {
+
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = start; i <= end; i++) {
+
+            int rob  = nums[i] + prev2;
+            int skip = prev1;
+
+            int curr = max(rob, skip);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+    int rob(vector<int>& nums) {
+        // memo.resize(nums.size(), -1);
+        // return dfs(nums, 0);
+
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        // int prev2 = 0;                // dp[i-2]
+        // int prev1 = 0;  // dp[i-1]
+        // int rob=0,skip=0,curr=0;
+        // for (int i = 0; i <= nums.size(); i++) {
+        //      //rob = prev2 + nums[i];
+        //     // skip = prev1;
+        //      curr = max(prev1, prev2 + nums[i]);
+        //             prev2 = prev1;
+        // prev1 = curr;
+        // }
+ 
+        return solve(nums,0,n-1);
+        // vector<int>dp(n);
+        // dp[0]=nums[0];
+        // dp[1]=max(nums[0],nums[1]);
+        // for(int i=2;i<nums.size();i++){
+        //     dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        // }
+        // return dp[n-1];
+    }
+};
