760257583dda624935bb26e097f85805564151fa
Mohamed_Salem <m7amed.salem@gmail.com>
2026-09-23 20:35:17 +0300

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        for(int L=0;L<nums.size();L++){
+            int len=min(static_cast<int>(nums.size()),L+k+1);
+            for(int R=L+1;R<nums.size();R++){
+                if((nums[R]==nums[L])&&((R-L)<=k)) return true;
+            }
+        }
+        return false;
+
+        // int L = 0;
+        // unordered_set<int> myset;
+        // for (int R = 0; R < nums.size(); R++) {
+        //     if (R - L > k) {
+        //         myset.erase(nums[L]);
+        //         L++;
+        //     }
+        //     if (myset.count(nums[R])) {
+        //         return true;
+        //     }
+        //     myset.insert(nums[R]);
+        // }
+        // return false;
+    }
+};
